edef433dab9b3a8d06871aa593c1af51dbbe7fec
Darshak Shah <darshak.shah@finastra.com>
2021-10-26 14:15:22 +0530

    to get connection # for max, normal, superuser, used

---
+++ sql/conn_count.sql
@@ -0,0 +1,14 @@
+-- Purpose : to get connection # for max, normal, superuser, used
+-- source : https://stackoverflow.com/questions/48619772/how-to-get-client-machine-name-ip-address-connecting-to-postgresql-server
+
+select max_conn, used, res_for_super, max_conn - used - res_for_super res_for_normal 
+from (
+  select count(*) used from pg_stat_activity
+) t1,
+(select setting::int res_for_super 
+ from pg_settings 
+ where name=$$superuser_reserved_connections$$
+) t2,
+(select setting::int max_conn 
+ from pg_settings 
+ where name=$$max_connections$$) t3;
